7fcfc4b75c43112225bfefaf2d0f098ef0979ca2
Swagata Chanda <swagata.chanda2000@gmail.com>
2022-01-17 12:19:24 +0530

    User CRUD added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body, Get } from '@nestjs/common'
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common'
+import { ObjectId } from 'mongoose'
 import { UserService } from './user.service'
 
 @Controller('user')
@@ -16,5 +17,28 @@ export class UserController {
     @Get('/all')
     getAllUser() {
         return this.userService.getAllUser()
+    }
+
+    @Get('/:id')
+    getUserbyId(@Param('id') id: ObjectId) {
+        return this.userService.getUserbyId(id)
+    }
+
+    @Put()
+    updateUserbyId(@Body() { id, ...userBody }: Record<string, any>) {
+        return this.userService.updateUserbyId(id, userBody)
+    }
+
+    @Put('/:id')
+    updateUser(
+        @Body() userBody: Record<string, any>,
+        @Param('id') id: ObjectId
+    ) {
+        return this.userService.updateUser(id, userBody)
+    }
+
+    @Delete('/:id')
+    deleteUserbyId(@Param('id') id: ObjectId) {
+        return this.userService.deleteUserbyId(id)
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common'
+import {
+    Injectable,
+    NotFoundException,
+    BadRequestException,
+} from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { User, UserDocument } from './user.model'
-import { Model } from 'mongoose'
+import { Model, ObjectId } from 'mongoose'
 
 @Injectable()
 export class UserService {
@@ -17,5 +21,80 @@ export class UserService {
 
     async getAllUser(): Promise<UserDocument[]> {
         return this.userModel.find()
+    }
+
+    async getUserbyId(id: ObjectId): Promise<UserDocument> {
+        const findUser = await this.userModel.findById(id)
+        if (!findUser) {
+            throw new NotFoundException(`User not found with id ${id}`)
+        }
+        return findUser
+    }
+
+    async updateUserbyId(
+        id: ObjectId,
+        userBody: Record<string, any>
+    ): Promise<Record<string, any>> {
+        const updateUser = await this.userModel.updateOne(
+            {
+                _id: id,
+            },
+            {
+                ...userBody,
+            }
+        )
+        if (updateUser.matchedCount === 0) {
+            throw new NotFoundException(`User with id ${id}`)
+        } else {
+            if (updateUser.modifiedCount === 0) {
+                throw new BadRequestException(
+                    `Update unsuccessful with userid ${id}`
+                )
+            }
+        }
+        return {
+            success: true,
+            message: `User id with ${id} updated successfully`,
+        }
+    }
+
+    async updateUser(
+        id: ObjectId,
+        userBody: Record<string, any>
+    ): Promise<Record<string, any>> {
+        const updateUser = await this.userModel.updateOne(
+            {
+                _id: id,
+            },
+            {
+                ...userBody,
+            }
+        )
+        if (updateUser.matchedCount === 0) {
+            throw new NotFoundException(`User with id ${id}`)
+        } else {
+            if (updateUser.modifiedCount === 0) {
+                throw new BadRequestException(
+                    `Update unsuccessful with userid ${id}`
+                )
+            }
+        }
+        return {
+            success: true,
+            message: `User id with ${id} updated successfully`,
+        }
+    }
+
+    async deleteUserbyId(id: ObjectId): Promise<Record<string, any>> {
+        const deleteUser = await this.userModel.deleteOne({ _id: id })
+        if (deleteUser.deletedCount === 0) {
+            throw new BadRequestException(
+                `User id with ${id} is either not found or delete operation unsuccessfull`
+            )
+        }
+        return {
+            success: true,
+            message: `User with id ${id} deleted successfully`,
+        }
     }
 }
